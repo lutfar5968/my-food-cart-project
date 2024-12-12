75637810e6dd16e370f597da64bb51e0f11e34f4
lutfar5968 <lutfar596800@gmail.com>
2024-12-12 13:22:31 +0600

    first xommit

---
+++ assests/custom.js
@@ -0,0 +1,456 @@
+
+let categories = 
+    [
+        {
+          "id": 48,
+          "name": "fast food",
+          "created_at": "2024-09-25T06:17:49.000000Z",
+          "updated_at": "2024-09-25T06:17:49.000000Z"
+        },
+        {
+          "id": 5,
+          "name": "healthy",
+          "created_at": "2024-09-25T06:20:09.000000Z",
+          "updated_at": "2024-09-25T06:20:09.000000Z"
+        },
+        {
+          "id": 52,
+          "name": "chicken fry",
+          "created_at": "2024-09-25T06:20:42.000000Z",
+          "updated_at": "2024-09-25T06:20:42.000000Z"
+        },
+        {
+        "id": 51,
+        "name": "Tandoori Chicken",
+        "created_at": "2024-09-10T09:51:27.000000Z",
+        "updated_at": "2024-09-10T09:51:27.000000Z"
+        },
+        
+];
+
+let favoriteFoods = [
+    {
+        "id": 82,
+        "category_id": 48,
+        "image": "https://course.divinecoder.com/images/1727248209.jpg",
+        "name": "burger",
+        "price": "80",
+        "rating": "65",
+        "rating_count": "780",
+        "facilities": "best food",
+        "category_name": "burger"
+      },
+      {
+        "id": 88,
+        "category_id": 51,
+        "image": "https://course.divinecoder.com/images/1727248664.jpg",
+        "name": "Tandoori Chicken",
+        "price": "456",
+        "rating": "76",
+        "rating_count": "89",
+        "facilities": "it is harmful for body",
+        "category_name": "Tandoori Chicken"
+      },
+      {
+        "id": 65,
+        "category_id": 51,
+        "image": "https://course.divinecoder.com/images/1727245888.jpg",
+        "name": "Tandoori Chicken",
+        "price": "600",
+        "rating": "300",
+        "rating_count": "500",
+        "facilities": "Its desi chicken",
+        "category_name": "Tandoori Chicken"
+      },
+      {
+        "id": 78,
+        "category_id": 48,
+        "image": "https://course.divinecoder.com/images/1727247848.jpg",
+        "name": "burger",
+        "price": "4",
+        "rating": "21",
+        "rating_count": "345",
+        "facilities": "Its testfull",
+        "category_name": "burger"
+      },
+      {
+        "id": 87,
+        "category_id": 48,
+        "image": "https://course.divinecoder.com/images/1727248588.jpg",
+        "name": "burger",
+        "price": "250",
+        "rating": "25",
+        "rating_count": "250",
+        "facilities": "best food",
+        "category_name": "burger"
+      },
+      {
+        "id": 80,
+        "category_id": 51,
+        "image": "https://course.divinecoder.com/images/1727248022.jpg",
+        "name": "Tandoori Chicken",
+        "price": "120",
+        "rating": "45",
+        "rating_count": "54",
+        "facilities": "very expensive",
+        "category_name": "Tandoori Chicken"
+      }
+]
+
+
+
+let FoodItems = [
+
+
+        {
+          "id": 75,
+          "category_id": 52,
+          "image": "https://course.divinecoder.com/images/1727247592.jpeg",
+          "name": "chicken Fry",
+          "price": "879",
+          "rating": "567",
+          "rating_count": "345",
+          "facilities": "Its testfull",
+          "category_name": "Chicken Fry"
+        },
+        {
+          "id": 86,
+          "category_id": 51,
+          "image": "https://course.divinecoder.com/images/1727248426.jpg",
+          "name": "Tandoori Chicken",
+          "price": "76",
+          "rating": "66",
+          "rating_count": "666",
+          "facilities": "food",
+          "category_name": "Tandoori Chicken"
+        },
+        {
+          "id": 92,
+          "category_id": 52,
+          "image": "https://course.divinecoder.com/images/1727249181.jpeg",
+          "name": "chicken Fry",
+          "price": "34",
+          "rating": "45",
+          "rating_count": "67",
+          "facilities": "Ek glass Pani, Duta Tissue, Ekta Box, Ekta sos, Duita Camoch",
+          "category_name": "Chicken Fry"
+        },
+        {
+          "id": 79,
+          "category_id": 52,
+          "image": "https://course.divinecoder.com/images/1727247937.jpg",
+          "name": "chicken Fry",
+          "price": "34",
+          "rating": "54",
+          "rating_count": "657",
+          "facilities": "it is harmful for body",
+          "category_name": "Chicken Fry"
+        },
+        {
+          "id": 69,
+          "category_id": 51,
+          "image": "https://course.divinecoder.com/images/1727246728.jpg",
+          "name": "Tandoori Chicken",
+          "price": "678",
+          "rating": "45",
+          "rating_count": "34",
+          "facilities": "food",
+          "category_name": "Tandoori Chicken"
+        },
+        {
+          "id": 70,
+          "category_id": 48,
+          "image": "https://course.divinecoder.com/images/1727247108.jpg",
+          "name": "burger",
+          "price": "344",
+          "rating": "54",
+          "rating_count": "567",
+          "facilities": "best food",
+          "category_name": "burger"
+        },
+        {
+          "id": 83,
+          "category_id": 48,
+          "image": "https://course.divinecoder.com/images/1727248293.jpg",
+          "name": "cheese burger",
+          "price": "76",
+          "rating": "43",
+          "rating_count": "234",
+          "facilities": "Its testfull",
+          "category_name": "burger"
+        },
+        {
+          "id": 74,
+          "category_id": 51,
+          "image": "https://course.divinecoder.com/images/1727247513.jpg",
+          "name": "Tandoori Chicken",
+          "price": "45",
+          "rating": "675",
+          "rating_count": "54",
+          "facilities": "food",
+          "category_name": "Tandoori Chicken"
+        },
+        {
+          "id": 81,
+          "category_id": 52,
+          "image": "https://course.divinecoder.com/images/1727248137.jpg",
+          "name": "chicken Fry",
+          "price": "4",
+          "rating": "56",
+          "rating_count": "23",
+          "facilities": "it is too testy to eat",
+          "category_name": "Chicken Fry"
+        },
+        {
+          "id": 76,
+          "category_id": 52,
+          "image": "https://course.divinecoder.com/images/1727247710.jpg",
+          "name": "chicken Fry",
+          "price": "6",
+          "rating": "45",
+          "rating_count": "980",
+          "facilities": "best food",
+          "category_name": "Chicken Fry"
+        },
+        {
+          "id": 66,
+          "category_id": 52,
+          "image": "https://course.divinecoder.com/images/1727246227.jpg",
+          "name": "chicken Fry",
+          "price": "300",
+          "rating": "99",
+          "rating_count": "56",
+          "facilities": "food",
+          "category_name": "Chicken Fry"
+        },
+        {
+          "id": 91,
+          "category_id": 48,
+          "image": "https://course.divinecoder.com/images/1727249095.jpg",
+          "name": "burger",
+          "price": "600",
+          "rating": "556",
+          "rating_count": "665",
+          "facilities": "food",
+          "category_name": "burger"
+        },
+        {
+          "id": 77,
+          "category_id": 51,
+          "image": "https://course.divinecoder.com/images/1727247773.jpg",
+          "name": "Tandoori Chicken",
+          "price": "160",
+          "rating": "89",
+          "rating_count": "564",
+          "facilities": "Its desi chicken",
+          "category_name": "Tandoori Chicken"
+        },
+        {
+          "id": 82,
+          "category_id": 48,
+          "image": "https://course.divinecoder.com/images/1727248209.jpg",
+          "name": "burger",
+          "price": "80",
+          "rating": "65",
+          "rating_count": "780",
+          "facilities": "best food",
+          "category_name": "burger"
+        },
+        {
+          "id": 88,
+          "category_id": 51,
+          "image": "https://course.divinecoder.com/images/1727248664.jpg",
+          "name": "Tandoori Chicken",
+          "price": "456",
+          "rating": "76",
+          "rating_count": "89",
+          "facilities": "it is harmful for body",
+          "category_name": "Tandoori Chicken"
+        },
+        {
+          "id": 65,
+          "category_id": 51,
+          "image": "https://course.divinecoder.com/images/1727245888.jpg",
+          "name": "Tandoori Chicken",
+          "price": "600",
+          "rating": "300",
+          "rating_count": "500",
+          "facilities": "Its desi chicken",
+          "category_name": "Tandoori Chicken"
+        },
+        {
+          "id": 78,
+          "category_id": 48,
+          "image": "https://course.divinecoder.com/images/1727247848.jpg",
+          "name": "burger",
+          "price": "4",
+          "rating": "21",
+          "rating_count": "345",
+          "facilities": "Its testfull",
+          "category_name": "burger"
+        },
+        {
+          "id": 87,
+          "category_id": 48,
+          "image": "https://course.divinecoder.com/images/1727248588.jpg",
+          "name": "burger",
+          "price": "250",
+          "rating": "25",
+          "rating_count": "250",
+          "facilities": "best food",
+          "category_name": "burger"
+        },
+        {
+          "id": 80,
+          "category_id": 51,
+          "image": "https://course.divinecoder.com/images/1727248022.jpg",
+          "name": "Tandoori Chicken",
+          "price": "120",
+          "rating": "45",
+          "rating_count": "54",
+          "facilities": "very expensive",
+          "category_name": "Tandoori Chicken"
+        }
+      
+];
+
+
+
+
+
+
+
+
+menu_Responsive();
+getCategoriesHandler();
+favoriteFoodHandler();
+foodItemByCategoryHandler();
+
+
+
+
+
+
+
+function menu_Responsive() {
+    
+let menuToggle = document.getElementById('menu-toggle');
+let menuList = document.getElementById('menu-list');
+
+menuToggle.addEventListener('click', function() {
+    menuList.classList.toggle('active');
+    
+});
+}
+
+
+
+
+async function getCategoriesHandler() {
+    let categoryElem = function (name, id) {
+       return `<li><a data-id="${id}" href="#">${name}</a></li>` ;
+    }
+
+
+    // let ulElement = document.getElementById('category_list');
+
+
+    let finalElementList = categories.map(item => categoryElem(item.name, item.id));
+
+    document.getElementById('category_list').innerHTML = finalElementList.join('');
+
+}
+
+
+function favoriteFoodHandler() {
+
+    favoriteFoods = favoriteFoods.map(item => {
+        return {
+            ...item,
+            isAddedToCart: true,
+        }
+    })
+
+
+    let favoriteItemElementWithData = favoriteFoods.map(food => foodCartElement(food));
+    document.getElementById('food_gallery').innerHTML = favoriteItemElementWithData.join('');
+}   
+
+function foodCartElement(food) {
+    return `<div class="col-lg-4 col-md-6 mb-4">
+                <div class="food-cart-item">
+                    <div class="img position-relative">
+                        <img class="img-fluid w-100" src="${food.image}" alt="image"/>
+                        <div class="overlay">
+                        <span>Price: ${food.price}/=</span>
+                        <i class="fa-solid fa-star"></i>
+                        <span>${food.rating} (${food.rating_count})</span>
+                        </div>
+                    </div>
+                    <div class="text-part">
+                        <h4>${food.category_name}</h4>
+                        <p>${food.category_name}</p>
+                        <ul class="list-unstyled d-flex flex-wrap">
+                        <li>Chili sauce</li>
+                        <li>Chili sauce</li>
+                        <li>Chili sauce</li>
+                        <li>Chili sauce</li>
+                        <li>Chili sauce</li>
+                        <li>Chili sauce</li>
+                        </ul>
+                    </div>
+                    <div class="${food.isAddedToCart == true ? 'active' : ''} add-to-cart">
+                        <a data-id="${food.id}" href="#">
+                        <i class="fa-solid fa-cart-plus"></i>
+                        <span>Add To Cart</span>
+                        </a>
+                    </div>
+                </div>
+      </div>`
+} 
+
+
+
+function foodItemByCategoryHandler() {
+    let buttons = document.querySelectorAll('.spacial-menu ul li a');
+
+    Array.from(buttons).forEach(function(btn) {
+
+        
+
+        btn.addEventListener('click', function (event) {
+
+            
+
+            let buttonId = null;
+            event.preventDefault();
+            Array.from(buttons).map(item =>{
+            item.classList.remove('active');
+            });
+            event.target.classList.add('active');
+
+
+            let id = event.target.getAttribute('data-id');
+
+            buttonId = id;
+
+            let updateFilterData = FoodItems.filter(function(item) {
+                return item.category_id == id;
+            });
+
+
+            let foodElementsAfterFilter = updateFilterData.map(food => foodCartElement(food));
+            
+            document.getElementById('food_gallery').innerHTML = foodElementsAfterFilter.join('');
+            
+            
+        })
+    })
+    
+}
+
+
+
+
+
+
